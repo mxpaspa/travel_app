3f55260913cd1851b1a6605c90fb2b3e4e97c8c2
mxpaspa <mxpaspa@gmail.com>
2017-03-01 19:40:49 -0500

    added bootstrap and contact page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    @homeland = 'United States'
+    @images = ['Croatia.jpg', 'Canada.jpg', 'Mexico.jpg', 'Germany.jpg']
   end
 
   def about
+    @color = params[:color]
+  end
+
+
+  def contact
   end
 end
